b52e3956befdb09d3eb317a28d40570352b892cb
Marlon Harrison <marlon@ionic.io>
2024-01-10 07:29:11 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Cap Keyboard Issue (Android)
+
+## Description
+
+A simple project to replicate an issue with the keyboard on Android devices. See <https://github.com/ionic-team/capacitor-plugins/issues/416>
+
+## Installation
+
+Provide step by step series of examples and explanations about how to get a development environment running.
+
+```bash
+npm install
+ionic build
+npx cap run android
+```
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonInput, IonIcon, IonLabel, IonListHeader } from '@ionic/angular/standalone';
+import { Keyboard } from '@capacitor/keyboard';
+
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonInput, IonIcon, IonLabel, IonListHeader],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {
+    Keyboard.addListener("keyboardDidShow", () => console.log('keyboardDidShow', window.innerWidth, window.innerHeight));
+  }
 }
